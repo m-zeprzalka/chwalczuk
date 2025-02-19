ae2ff3d83c823070291ae5325cc7d6e808dd9084
m-zeprzalka <m@zeprzalka.pl>
2025-02-19 13:01:04 +0100

    Remove unused portfolios and add ESLint ignore

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,4 @@
 import Portfolio from "@/components/Portfolio";
-import PortfolioB from "@/components/PortfolioB";
-import PortfolioC from "@/components/PortfolioC";
-import PortfolioD from "@/components/PortfolioD";
-import PortfolioE from "@/components/PortfolioE";
 
 export default function Home() {
   return <Portfolio />;
